246590292724bef9ce6eab2a54d18d3ba7ccd905
Peris Wakio <peris.kingori@student.moringaschool.com>
2023-08-13 13:14:40 -0700

    completed assignment

--- index.js
+++ index.js
@@ -38,8 +38,9 @@ const main = document.getElementById("main");
 main.remove()
 
 const newHeader = document.createElement("h1");
-document.body.append(newHeader);
+
 newHeader.id = "victory"
 
-newHeader.textContent = "YOUR-NAME is the champion"
+newHeader.textContent = "peris is the champion"
 
+document.body.append(newHeader);
